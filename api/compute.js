b4ba4683b601dba189ea1e37a519038d4e992328
Thando Hlomuka <thando@metramarket.co.za>
2026-05-31 16:42:23 +0200

    Rename api/data.js -> _data.js to prevent it becoming a separate lambda, update all requires

--- api/compute.js
+++ api/compute.js
@@ -1,4 +1,4 @@
-const data = require('./data');
+const data = require('./_data');
 
 /* ─── Embedded Finance Engine (server-side) ─── */
 const Finance = {
--- api/reports.js
+++ api/reports.js
@@ -1,4 +1,4 @@
-const data = require('./data');
+const data = require('./_data');
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
